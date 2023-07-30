c94dd10afa5f3574fe046f8cffcfe84d16355526
huzaifak08 <hk7928042@gmail.com>
2023-07-30 21:29:44 +0500

    Update and Delete done Allhamdullillah

--- Database/connection.js
+++ Database/connection.js
@@ -4,6 +4,7 @@ mongoose.connect("mongodb://localhost:27017/restfull-api", {
     // useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    // useFindAndModify:false,
 }).then(() => {
     console.log("MondoDB Connection is Successfull");
 }).catch((e) => {
--- index.js
+++ index.js
@@ -65,7 +65,38 @@ app.get("/students/:id", async (req, res) => {
     } catch (e) {
         res.status(500).send(e.message); // 500 : Server Error
     }
-})
+});
+
+// Delete student Data:
+app.delete("/students/:id", async (req, res) => {
+    try {
+
+        const delStu = await Student.findByIdAndDelete(req.params.id);
+        if (!req.params.id) {
+            return res.status(404).send();
+        } else {
+            res.send(delStu);
+        }
+
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
+
+// Update/Patch Student Data:
+app.patch("/students/:id", async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const updStu = await Student.findByIdAndUpdate(_id, req.body, {
+            new: true,
+        });
+
+        res.status(200).send(updStu);
+
+    } catch (err) {
+        res.status(404).send(err.message);
+    }
+});
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
