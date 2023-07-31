8649e0abf5a9083365a057ef8d4ec66e500c2da7
huzaifak08 <hk7928042@gmail.com>
2023-07-31 23:07:05 +0500

    JWT creation done Allhamdullillah

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require("./Database/connection");
-const Student = require("./Models/student_model");
 const studentRouter = require("./Routers/student");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -10,6 +10,20 @@ app.use(express.json()); // recognize incoming request as JSON object(POST)
 
 // Calling the router
 app.use(studentRouter);
+
+// Create JSON Web Token:
+const createToken = async () => {
+    const token = await jwt.sign({ _id: "64c5496ef177ed2cfc3cd59a" }, "islamislamislamislamislamislamislamislam", {
+        expiresIn: "2 seconds"  // Not Mandetory
+    });
+    console.log(token);
+
+    // Verify Token:
+    const userVer = await jwt.verify(token, "islamislamislamislamislamislamislamislam");
+    console.log(userVer); // Returns Id when token is correct
+}
+
+createToken();
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
